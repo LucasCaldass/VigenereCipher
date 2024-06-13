4e32176c89b6bdd8eeceb0183e8503a195751000
Lucas <lucasdeamorimcaldas@gmail.com>
2024-06-12 22:03:45 -0300

    Fixed the problem where the input couldn't have blank spaces in it, now, it's possible to input sentences with blank spaces in it in order to cryptograph/decryptograph

--- decrypt.py
+++ decrypt.py
@@ -5,15 +5,27 @@ def decrypt(curr_key, curr_phrase):
     curr_key = curr_key.upper()
     curr_phrase = curr_phrase.upper()
     completed_key = complete_key(curr_key, len(curr_phrase))
+    size = len(completed_key)
 
     decrypted_phrase = ''
 
-    for char_phrase, char_key in zip(curr_phrase, completed_key):
-        if char_phrase.isalpha():
-            shift = ord(char_key) - ord('A')
-            decrypted_char = chr(((ord(char_phrase) - ord('A') - shift) % 26) + ord('A'))
-            decrypted_phrase += decrypted_char
+    index_phrase = 0
+    index_key = 0
+
+    while index_phrase < size:
+        if curr_phrase[index_phrase] == ' ':
+            decrypted_phrase += curr_phrase[index_phrase]
+            index_phrase += 1
+        elif curr_phrase[index_phrase].isalpha():
+            c = ord(curr_phrase[index_phrase]) - ord('A')
+            k = ord(completed_key[index_key]) - ord('A')
+            decrypted_phrase += chr(((c - k) % 26) + ord('A'))
+
+            index_phrase += 1
+            index_key += 1
         else:
-            decrypted_phrase += char_phrase
+            decrypted_phrase += curr_phrase[index_phrase]
+            index_phrase += 1
+            index_key += 1
 
     return decrypted_phrase
--- encrypt.py
+++ encrypt.py
@@ -4,16 +4,28 @@ from setKeyLength import complete_key
 def encrypt(curr_key, curr_phrase):
     curr_key = curr_key.upper()
     curr_phrase = curr_phrase.upper()
-    completed_key = complete_key(curr_key, len(curr_phrase))  # Modificando o tamanho da chave para ser o mesmo da frase
+    completed_key = complete_key(curr_key, len(curr_phrase))
+    size = len(completed_key)
 
     encrypted_phrase = ''
 
-    for char_phrase, char_key in zip(curr_phrase, completed_key):
-        if char_phrase.isalpha():
-            shift = ord(char_key) - ord('A')
-            encrypted_char = chr(((ord(char_phrase) - ord('A') + shift) % 26) + ord('A'))
-            encrypted_phrase += encrypted_char
+    index_phrase = 0
+    index_key = 0
+
+    while index_phrase < size:
+        if curr_phrase[index_phrase] == ' ':
+            encrypted_phrase += curr_phrase[index_phrase]
+            index_phrase += 1
+        elif curr_phrase[index_phrase].isalpha():
+            p = ord(curr_phrase[index_phrase]) - ord('A')
+            k = ord(completed_key[index_key]) - ord('A')
+            encrypted_phrase += chr(((p + k) % 26) + ord('A'))
+
+            index_phrase += 1
+            index_key += 1
         else:
-            encrypted_phrase += char_phrase
+            encrypted_phrase += curr_phrase[index_phrase]
+            index_phrase += 1
+            index_key += 1
 
     return encrypted_phrase
